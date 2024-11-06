2b1f40d775c418506a6e2d7ea77fad1bd5982388
omarc <omarc@ItsMe>
2024-11-06 10:01:06 +0100

    Version 2.0

--- kredyt_03/src/main/java/com/jsfcourse/calc/KredytBB.java
+++ kredyt_03/src/main/java/com/jsfcourse/calc/KredytBB.java
@@ -52,50 +52,7 @@ public class KredytBB {
 		this.result = result;
 	}
 	
-	//
-	private boolean validateInput() {
-		try {
-			double kwota = Double.parseDouble(this.kwota);
-			if (kwota < 5000) {
-				ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Kwota pozyczki musi wynosić conajmniej 5000 PLN", null));
-				return false;
-			}
-		} catch (NumberFormatException e) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Kwota musi być liczbą", null));
-			return false;
-		}
-
-		try {
-			double ilelat = Double.parseDouble(this.ilelat);
-			if (ilelat <= 0) {
-				ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Liczba lat musi być większa od zera", null));
-				return false;
-			}
-		} catch (NumberFormatException e) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Liczba lat musi być liczbą", null));
-			return false;
-		}
-
-		try {
-			double oprocentowanie = Double.parseDouble(this.oprocentowanie);
-			if (oprocentowanie < 0) {
-				ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Oprocentowanie nie może być ujemne", null));
-				return false;
-			}
-		} catch (NumberFormatException e) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Oprocentowanie musi być liczbą", null));
-			return false;
-		}
-
-		return true;
-	}
-	//
-
 	public boolean doTheMath() {
-		if (!validateInput()) {
-			return false;
-		}
-		
 		try {
 			double kwota = Double.parseDouble(this.kwota);
 			double ilelat = Double.parseDouble(this.ilelat);
@@ -112,8 +69,7 @@ public class KredytBB {
 			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
 			return true;
 		} catch (Exception e) {
-			ctx.addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
 			return false;
 		}
 	}
@@ -130,12 +86,8 @@ public class KredytBB {
 	public String calc_AJAX() {
 		if (doTheMath()) {
 			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Twoja rata kredytu wynosi: " + result, null));
-
 		}
 		return null;
 	}
 
-	public String info() {
-		return "info";
-	}
 }
