35775cb5fa6b7e218ce649ffe592165e56b79bc7
Marek Otulak <omarco2109@gmail.com>
2024-12-01 01:23:45 +0100

    Version 1.0.2

--- BeatLoop/src/main/java/com/BeatLoop/ctrl/BeginCtrl.java
+++ BeatLoop/src/main/java/com/BeatLoop/ctrl/BeginCtrl.java
@@ -1,5 +1,20 @@
 package com.BeatLoop.ctrl;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/BeginCtrl")
+public class BeginCtrl extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("BeginCtrl działa poprawnie");
+        resp.getWriter().write("BeginCtrl działa poprawnie");
+    }
+}
+
 
 
 /*
---
+++ BeatLoop/src/main/java/com/BeatLoop/user/UserList.java
@@ -0,0 +1,92 @@
+package com.BeatLoop.user;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ejb.EJB;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.Flash;
+import jakarta.servlet.http.HttpSession;
+
+import com.BeatLoop.dao.UserDAO;
+import com.BeatLoop.entities.User;
+
+@Named
+@RequestScoped
+public class UserList {
+	private static final String PAGE_PERSON_EDIT = "personEdit?faces-redirect=true";
+	private static final String PAGE_STAY_AT_THE_SAME = null;
+
+	private String surname;
+		
+	@Inject
+	ExternalContext extcontext;
+	
+	@Inject
+	Flash flash;
+	
+	@EJB
+	UserDAO personDAO;
+		
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public List<User> getFullList(){
+		return personDAO.getFullList();
+	}
+
+	public List<User> getList(){
+		List<User> list = null;
+		
+		//1. Prepare search params
+		Map<String,Object> searchParams = new HashMap<String, Object>();
+		
+		if (surname != null && surname.length() > 0){
+			searchParams.put("surname", surname);
+		}
+		
+		//2. Get list
+		list = personDAO.getList(searchParams);
+		
+		return list;
+	}
+
+	public String newUser(){
+		User user = new User();
+		
+		//1. Pass object through session
+		//HttpSession session = (HttpSession) extcontext.getSession(true);
+		//session.setAttribute("person", person);
+		
+		//2. Pass object through flash	
+		flash.put("user", user);
+		
+		return PAGE_PERSON_EDIT;
+	}
+
+	public String editUser(User user){
+		//1. Pass object through session
+		//HttpSession session = (HttpSession) extcontext.getSession(true);
+		//session.setAttribute("person", person);
+		
+		//2. Pass object through flash 
+		flash.put("user", user);
+		
+		return PAGE_PERSON_EDIT;
+	}
+
+	public String deleteUser(User user){
+		personDAO.remove(user);
+		return PAGE_STAY_AT_THE_SAME;
+	}
+}
